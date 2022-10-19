3dff999aca92662cedfbccaf0cb987c449b1c4f1
luandalmazo <luandalmazo@gmail.com>
2022-10-18 22:18:55 -0300

    add pedro patchnote v1

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import os
 import telebot
 from bs4 import BeautifulSoup
 import requests
-from threading import Thread
-import schedule
-from time import sleep
+import urllib.request
+import json
 
 ''' Configs '''
-TOKEN = ""
+TOKEN = "YOUR_TOKEN"
 bot = telebot.TeleBot(TOKEN)
 
 comandos = ["*/apresentar* - Apresenta o bot",
@@ -23,7 +22,9 @@ comandos = ["*/apresentar* - Apresenta o bot",
   "*/acervo* - Envia o link do acervo de conteúdos do curso",
   "*/sugestoes* - Envia sugestões de funcionalidades para o bot | Envie /sugestão + sua sugestão",
   "*/discord* - Envia o link do discord do curso",
-  "*/ensalamento* - Envia o link do ensalamento do curso"]
+  "*/ensalamento* - Envia o link do ensalamento do curso",
+  "*/pesquisarsala* - Procura pelo ensalamento de uma discíplina | Envie /pesquisar_sala + nome da disciplina (ou código)",
+  "*/man* - Instruções sobre comandos"]
 
 ''' Commands'''
 @bot.message_handler(commands=['apresentar'])
@@ -136,7 +137,6 @@ def ru(message):
         i += 1
 
     bot.reply_to(message, answer, parse_mode="Markdown")
-    print(message.chat.id)
     
 @bot.message_handler(commands=['eventos'])
 def eventos(message):
@@ -197,15 +197,111 @@ def ensalamento(message):
     bot.reply_to(message, "Aqui está o link para o ensalamento")
     bot.reply_to(message, "https://ensalamento.c3sl.ufpr.br/")
 
-def schedule_checker():
-    while True:
-        schedule.run_pending()
-        sleep(1)
+@bot.message_handler(commands=['pesquisarsala'])
+def pesquisar_sala(message):
+    comando = message.text
+    comando = comando.replace("/pesquisarsala", "")
+    comando = comando.replace("@PedroRealBot", "")
+    comando = comando.replace(" ", "")
+    if comando == "":
+        bot.reply_to(message, "Envie o comando /pesquisarsala + nome da discíplina\nPrefira pesquisar pelo código da disciplina!")
+    else:
+        API = f"https://ensalamento.c3sl.ufpr.br/api/disciplinas/search?query={comando}"        
+        r = ""
+        try:
+            url = urllib.request.urlopen(API)
+            data = url.read()
+            json_data = json.loads(data)
+        except: 
+            bot.reply_to(message, "Ocorreu um erro!") 
 
-def anuncio():
-    return "something"
+        try:
+            for data in json_data["turmas"]:
+                r += "*"+data["nome"]+"*" + "\n"
+                for d in data["turmas"]:
+                    for p in d["professores"]:
+                        r += "Professores: "+p["nome"] + ","
+                    r+="\n"
+                    try:
+                        for h in d["horarios"]:
+                            r += "Horarios: "+str(h["dia"]) + "ª " + str(h["horario_inicial"]) + " - " + str(h["horario_final"]) + "\n"
+                            r += "Sala: "+h["salaCode"] + "\n"
+                            r += "\n"
+                    except:
+                        r += "Horarios: Não informado" + "\n"
+                        r += "Sala: Não informado" + "\n"
+                        r += "\n"
+            bot.reply_to(message, r, parse_mode="Markdown")
+        except:
+            bot.reply_to(message, "Disciplina não encontrada")  
+    
+@bot.message_handler(commands=['man'])
+def man(message):
+    comando = message.text
+    comando = comando.replace("/man", "")
+    comando = comando.replace("@PedroRealBot", "")
+    
+    r = ""
+    if comando == "":
+        r = "Envie o comando /man + nome do comando\n*Opções disponíveis:\n*"
+        r += "/man apresentar\n"
+        r += "/man grade\n"
+        r += "/man processo\n"
+        r += "/man novo processo\n"
+        r += "/man info\n"
+        r += "/man help\n"
+        r += "/man datas\n"
+        r += "/man ru\n"
+        r += "/man eventos\n"
+        r += "/man novoevento\n"
+        r += "/man acervo\n"
+        r += "/man sugestoes\n"
+        r += "/man discord\n"
+        r += "/man ensalamento\n"
+        r += "/man pesquisarsala\n"
+
+    elif comando.find("apresentar") != -1:
+        r = "Este comando apresenta o Pedro, e enviará o link do repositório que ele se encontra!"
+    elif comando.find("grade") != -1:
+        r = "Este comando enviará a grade do curso de Informática Biomédica por PDF!\n*Observação: este comando as vezes pode demorar alguns milisegundos para responder!*"
+    elif comando.find("processo") != -1:
+        r = "Este comando enviará a descrição de todos os processos seletivos abertos até o momento!"
+    elif comando.find("novoprocesso") != -1:
+        r = "Neste comando você pode acrescentar um novo processo seletivo.\nEnvie: /novoprocesso + descrição do processo seletivo\n"
+        r += "*Exemplo: /novoprocesso Processo seletivo para bolsista de extensão | Link: -- | Data: --*"
+    elif comando.find("info") != -1:
+        r = "Este comando enviará o link para o site do curso de Informática Biomédica que contém informações sobre o curso como um todo!"
+    elif comando.find("help") != -1:
+        r = "Este comando enviará uma lista de comandos que eu posso realizar!"
+    elif comando.find("datas") != -1:
+        r = "Este comando enviará fotos contendo as datas para o respectivo mês!\n"
+        r += "*Exemplo: /datas outubro*"
+        r += "\nObservação: envie o nome do mês por extenso!"
+    elif comando.find("ru") != -1:
+        r = "Este comando enviará o cardápio do RU para os próximos dias!\n"
+        r += "*Observação: este comando as vezes pode demorar alguns milisegundos para responder!*"
+    elif comando.find("eventos") != -1:
+        r = "Este comando enviará uma lista de eventos que estão acontecendo no curso!"
+    elif comando.find("novoevento") != -1:
+        r = "Neste comando você pode acrescentar um novo evento\nEnvie: /novoevento + descrição do evento\n"
+        r += "*Exemplo: /novoevento SABER | Link: -- | Data: --*"
+    elif comando.find("acervo") != -1:
+        r = "Este comando enviará o link para o acervo do curso de Informática Biomédica!"
+    elif comando.find("sugestoes") != -1:
+        r = "Neste comando você pode mandar uma sugestão para o Pedro!\nEnvie: /sugestoes + sua sugestão\n"
+        r += "*Exemplo: /sugestoes O Pedro deveria ter outro nome!*"
+    elif comando.find("discord") != -1:
+        r = "Este comando enviará o link para o servidor do Discord do curso de Informática Biomédica!"
+    elif comando.find("ensalamento") != -1:
+        r = "Este comando enviará o link para o site do ensalamento do C3SL!"
+    elif comando.find("pesquisarsala") != -1:
+        r = "Neste comando você pode pesquisar a sua sala!\nEnvie: /pesquisarsala + nome da disciplina"
+        r += "\n*Exemplo: /pesquisarsala ci1055\n*"
+        r += "\nObservação: Prefira pesquisar pelo código da disciplina, pelo nome posso ter dificuldades em achar!"
+    else:
+        r  = "Comando não encontrado"
+    bot.reply_to(message, r, parse_mode="Markdown")
+
 
 ''' Infinite loop to keep the bot running '''
-Thread(target=bot.polling).start()
-schedule.every().monday.at("10:00").do(anuncio)
-Thread(target=schedule_checker).start()
+bot.polling()
